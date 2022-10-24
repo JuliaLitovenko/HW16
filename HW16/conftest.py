61ecae005c849fc0081de5f728101f20a50926df
JuliaLitovenko <julia.litovenko@gmail.com>
2022-10-24 08:21:31 +0300

    Update conftest.py

--- HW16/conftest.py
+++ HW16/conftest.py
@@ -5,12 +5,7 @@ from selenium import webdriver
 subprocess.run("docker rm -f selenium_chrome", shell=True, check=True)
 
 port = 4444
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument('--headless')
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--disable-dev-shm-usage")
+
 
 @pytest.fixture(scope="module", autouse=True)
 def docker():
@@ -18,15 +13,21 @@ def docker():
                    f" {port}:4444 -v "
                    f"/dev/shm:/dev/shm selenium/standalone-chrome",
                    shell=True, check=True)
-    time.sleep(5)
-    pytest.driver = webdriver.Remote(
-        command_executor=f'http://localhost:{port}/wd/hub',
-        options=options
-    )
+time.sleep(5)
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--disable-dev-shm-usage")
+pytest.driver = webdriver.Remote(
+   command_executor=f'http://localhost:{port}/wd/hub',
+   options=options
+)
 
-    # Post-conditions
-    yield
-    subprocess.run("docker rm -f selenium_chrome", shell=True, check=True)
+  # Post-conditions
+yield
+subprocess.run("docker rm -f selenium_chrome", shell=True, check=True)
 
 
 
