0326eabb527f89cc83e72b82812a74879b3d251c
JuliaLitovenko <julia.litovenko@gmail.com>
2022-10-24 08:25:09 +0300

    Update conftest.py

--- HW16/conftest.py
+++ HW16/conftest.py
@@ -13,19 +13,19 @@ def docker():
                    f" {port}:4444 -v "
                    f"/dev/shm:/dev/shm selenium/standalone-chrome",
                    shell=True, check=True)
-time.sleep(5)
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument('--headless')
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--disable-dev-shm-usage")
-pytest.driver = webdriver.Remote(
-   command_executor=f'http://localhost:{port}/wd/hub',
-   options=options
-)
+    time.sleep(5)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--disable-dev-shm-usage")
+    pytest.driver = webdriver.Remote(
+        command_executor=f'http://localhost:{port}/wd/hub',
+        options=options
+        )
 
-  # Post-conditions
+    # Post-conditions
     yield
     subprocess.run("docker rm -f selenium_chrome", shell=True, check=True)
 
